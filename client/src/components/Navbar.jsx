020905040c23e335de08729b41dc18e39c89dfd4
fahlanlogic <alanfahdi@gmail.com>
2024-06-13 22:29:56 +0700

    add hero and set responsive design

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,20 +3,32 @@ import { BsTelephoneFill } from "react-icons/bs";
 import { IoMail } from "react-icons/io5";
 
 export default function Navbar() {
+  const scrollY = window.scrollY;
+  window.onscroll = () => {
+    let currentScrollY = window.scrollY;
+    if (currentScrollY > scrollY) {
+      document.querySelector("header").classList.add("bg-neutral-900");
+    } else {
+      document.querySelector("header").classList.remove("bg-neutral-900");
+    }
+  };
+
   return (
-    <header className="sticky top-0">
-      <nav className="py-3 mx-auto text-center space-y-3 bg-black text-white shadow-sm">
+    <header className="fixed w-full duration-500 top-0 z-50">
+      <nav
+        className="py-3 px-3 mx-auto text-center space-y-3  text-white shadow-sm md:flex justify-between items-center md:space-y-0 md:max-w-2xl lg:max-w-4xl
+      xl:max-w-6xl md:py-5 sm:px-0">
         <h1 className="font-bold">BatamDestination</h1>
-        <div className="flex w-full mx-auto text-sm justify-around">
+        <div className="flex w-full mx-auto text-xs sm:text-sm justify-around md:justify-end gap-6">
           <div className="flex items-center gap-2">
-            <div className="p-1 rounded-full bg-green-600 text-white text-xs">
-              <BsTelephoneFill className="" />
+            <div className="p-1 rounded-full bg-green-600 text-white text-[10px]">
+              <BsTelephoneFill />
             </div>
             <p>081188876677</p>
           </div>
           <div className="flex items-center gap-2">
-            <div className="p-1 rounded-full bg-green-600 text-white text-xs">
-              <IoMail className="" />
+            <div className="p-1 rounded-full bg-green-600 text-white text-[10px]">
+              <IoMail />
             </div>
             <p>batamdestination@gmail.com</p>
           </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,31 @@
 export default function Home() {
   return (
     <main className="h-[9999px]">
-      <h1>Home</h1>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1619880889144-d6e252999afa?q=80&w=3348&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+          height: "100vh",
+          backgroundAttachment: "fixed",
+          backgroundSize: "cover",
+        }}>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="md:max-w-2xl lg:max-w-3xl">
+            <h1 className="mb-10 text-6xl sm:text-7xl font-extrabold md:text-8xl lg:text-9xl">
+              Tour Lover in Batam
+            </h1>
+            <p className="mb-10 text-sm sm:text-base">
+              Menikmati pesona alam Batam: perpaduan pantai berpasir putih,
+              hutan hijau yang rimbun, dan matahari terbenam yang memukau.
+            </p>
+            <button className="bg-green-600 px-4 py-2 rounded-full text-neutral-900 font-semibold hover:scale-110 duration-500 text-sm sm:text-base">
+              Eksplorasi
+            </button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
