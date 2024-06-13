e99396206a1ee2f7fd76380339edfc96f9d70d55
fahlanlogic <alanfahdi@gmail.com>
2024-06-13 20:28:10 +0700

    edit color icons in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { RiWhatsappFill } from "react-icons/ri";
+// import { RiWhatsappFill } from "react-icons/ri";
+import { BsTelephoneFill } from "react-icons/bs";
 import { IoMail } from "react-icons/io5";
 
 export default function Navbar() {
@@ -7,12 +8,16 @@ export default function Navbar() {
       <nav className="py-3 mx-auto text-center space-y-3 bg-black text-white shadow-sm">
         <h1 className="font-bold">BatamDestination</h1>
         <div className="flex w-full mx-auto text-sm justify-around">
-          <div className="flex items-center gap-1">
-            <RiWhatsappFill className="text-lg" />
+          <div className="flex items-center gap-2">
+            <div className="p-1 rounded-full bg-green-600 text-white text-xs">
+              <BsTelephoneFill className="" />
+            </div>
             <p>081188876677</p>
           </div>
-          <div className="flex items-center gap-1">
-            <IoMail className="text-lg" />
+          <div className="flex items-center gap-2">
+            <div className="p-1 rounded-full bg-green-600 text-white text-xs">
+              <IoMail className="" />
+            </div>
             <p>batamdestination@gmail.com</p>
           </div>
         </div>
