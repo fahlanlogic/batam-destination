6c2d67c5b5632d0c7b3425f2a8f97dabd82fd170
fahlanlogic <alanfahdi@gmail.com>
2024-06-16 17:49:34 +0700

    create tour vehicle section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 // className="py-3 px-3 mx-auto text-center space-y-3  text-white shadow-sm md:flex justify-between items-center md:space-y-0 md:max-w-2xl lg:max-w-4xl xl:max-w-6xl md:py-5 sm:px-0"
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
@@ -91,67 +92,112 @@ export default function Home() {
               Tour Lover in Batam
             </h1>
             <p className="mb-10 text-sm sm:text-base">
-              Menikmati pesona alam Batam: perpaduan pantai berpasir putih,
-              hutan hijau yang rimbun, dan matahari terbenam yang memukau.
+              Enjoy Batam's natural charm, a combination of white sandy beaches,
+              lush green forests and stunning sunsets
             </p>
             <button className="bg-green-600 px-4 py-2 rounded-full text-neutral-900 font-semibold hover:scale-110 duration-500 text-sm sm:text-base">
-              Eksplorasi
+              Explore
             </button>
           </div>
         </div>
       </div>
 
       {/* Tour place */}
-      <div className="min-h-screen mx-auto text-center px-3 md:max-w-2xl lg:max-w-4xl xl:max-w-6xl md:px-0">
-        <h1 className="text-4xl md:text-5xl font-extrabold my-14">
-          Tour Place
-        </h1>
-        <div className="relative">
-          <Swiper
-            autoplay={true}
-            pagination={true}
-            breakpoints={{
-              640: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-              },
-              1024: {
-                slidesPerView: 4,
-                spaceBetween: 10,
-              },
-              0: {
-                slidesPerView: 1,
-                spaceBetween: 10,
-              },
-            }}>
-            {tourPlaces.map(listing => (
-              <SwiperSlide key={listing.id}>
-                <div
-                  className="relative card rounded-xl min-h-[470px]"
-                  style={{
-                    background: `url(${listing.image}) center no-repeat`,
-                    backgroundSize: "cover",
-                  }}>
-                  <div className="bg-neutral-100 w-full absolute bottom-0 p-3 text-left space-y-4 rounded-b-xl">
-                    <h3 className="font-bold text-lg">{listing.name}</h3>
-                    <p className="text-xs font-light line-clamp-2 md:text-sm text-neutral-500">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Sed aliquam pariatur a assumenda ad soluta, alias adipisci
-                      accusamus eum excepturi facilis officiis eaque? Eveniet
-                      corporis doloremque accusantium, at libero praesentium.
-                    </p>
-                    <div className="flex items-center gap-3">
-                      <p className="font-bold">Rp 10.000</p>
-                      <p className="font-light line-through text-xs text-neutral-500">
-                        Rp 50.000
+      <div className="h-screen flex items-center justify-center mx-auto text-center px-3 md:max-w-2xl lg:max-w-4xl xl:max-w-6xl md:px-0">
+        <div className="w-full">
+          <h1 className="text-4xl md:text-5xl font-extrabold my-14">
+            Tour Place
+          </h1>
+          <div className="relative">
+            <Swiper
+              autoplay={true}
+              pagination={true}
+              breakpoints={{
+                640: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                },
+                1024: {
+                  slidesPerView: 4,
+                  spaceBetween: 10,
+                },
+                0: {
+                  slidesPerView: 1,
+                  spaceBetween: 10,
+                },
+              }}>
+              {tourPlaces.map(listing => (
+                <SwiperSlide key={listing.id}>
+                  <div
+                    className="relative card rounded-xl min-h-[470px]"
+                    style={{
+                      background: `url(${listing.image}) center no-repeat`,
+                      backgroundSize: "cover",
+                    }}>
+                    <div className="bg-neutral-100 w-full absolute bottom-0 p-3 text-left space-y-4 rounded-b-xl">
+                      <h3 className="font-bold text-lg">{listing.name}</h3>
+                      <p className="text-xs font-light line-clamp-2 md:text-sm text-neutral-500">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Sed aliquam pariatur a assumenda ad soluta, alias
+                        adipisci accusamus eum excepturi facilis officiis eaque?
+                        Eveniet corporis doloremque accusantium, at libero
+                        praesentium.
                       </p>
+                      <div className="flex items-center gap-3">
+                        <p className="font-bold">Rp 10.000</p>
+                        <p className="font-light line-through text-xs text-neutral-500">
+                          Rp 50.000
+                        </p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
         </div>
+      </div>
+
+      {/* Tour Vehicle */}
+      <div className="min-h-screen mx-auto text-center px-3 md:max-w-2xl lg:max-w-4xl xl:max-w-6xl md:px-0">
+        <h1 className="text-4xl md:text-5xl font-extrabold my-14">
+          Tour Vehicle
+        </h1>
+        <div className="sm:py-7">
+          <div className="flex bg-green-600 py-2 rounded-full font-semibold text-neutral-800 text-sm w-fit mx-auto px-8">
+            <p className="pr-8">Daily</p>
+            <p className="border-x px-8 border-neutral-800">Weekly</p>
+            <p className="pl-8">Monthly</p>
+          </div>
+        </div>
+        <div className="flex justify-between my-10">
+          <img
+            src="../../public/avanza.png"
+            alt=""
+            className="h-28 sm:h-32 hidden sm:block md:h-32 lg:h-44 xl:h-52"
+          />
+          <img
+            src="../../public/innova.png"
+            alt=""
+            className="h-[122px] sm:h-32 md:h-32 lg:h-44 xl:h-52"
+          />
+          <img
+            src="../../public/hiace.png"
+            alt=""
+            className="h-[122px] sm:h-32 md:h-32 lg:h-44 xl:h-52"
+          />
+        </div>
+        <p className="text-sm text-neutral-700 leading-6">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+          delectus voluptates unde ipsam, blanditiis cumque laboriosam vitae
+          eveniet officiis ipsa officia perspiciatis numquam voluptatibus earum
+          veniam nostrum sunt. Ipsam, consequatur! Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Ut quibusdam cumque dolorum fugiat
+          temporibus totam facere assumenda anim.
+        </p>
+        <button className="text-sm font-semibold bg-green-600 rounded-full px-3 py-2 mt-7 hover:scale-110 duration-500">
+          Book Now
+        </button>
       </div>
     </main>
   );
